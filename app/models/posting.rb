b6a0a994967546b77099c30597ff1f6e2bc4e5c7
Mariia Potashnykova <completecarbon12@gmail.com>
2023-09-05 12:55:03 +0300

    Update posting.rb

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -11,6 +11,7 @@ class Posting < ApplicationRecord
     return "#{figure_start}_#{figure_end}" if figure_start.nil? || figure_end.nil?
 
     image_tags = body[figure_start...figure_end + 9]
+    # Avoid using magic numbers. 9 should be a constant with explanatory name
     return 'not include <img' unless image_tags.include?('<img')
 
     posting_image_params(image_tags)
@@ -26,9 +27,14 @@ class Posting < ApplicationRecord
       data = tag_parse.(html, attribute)
       unless data.nil?
         tag_attributes[attribute] = data[1] unless data.size < 2
+        # Avoid using magic numbers. 2 should be a constant with explanatory name
       end
     end
     # tag_parse
     tag_attributes
   end
 end
+
+# In case if article contains the image, its snippet should contain image above the body -- was implemented. 
+
+# Doesn't handle the case when article has multiple images
