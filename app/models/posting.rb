2dae43fad46cfd23ae7bd22571cc002be9a65c54
MariaYasinets <64211154+MariaYasinets@users.noreply.github.com>
2024-02-12 10:56:15 +0200

    Review Feedback

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -3,9 +3,10 @@ class Posting < ApplicationRecord
   belongs_to :author,    class_name: 'User', foreign_key: 'user_id'
   belongs_to :editor,    class_name: 'User', foreign_key: 'editor_id'
   
+  #I would move this piece of code to some helper or presenter. This logic is about the view representation and should be removed from the model at all costs.
   def article_with_image
     return type if type != 'Article'
-
+   
     figure_start = body.index('<figure')
     figure_end = body.index('</figure>')
     return "#{figure_start}_#{figure_end}" if figure_start.nil? || figure_end.nil?
@@ -18,12 +19,14 @@ class Posting < ApplicationRecord
 
   private
 
+  #This method should definitely be removed from the model. We are working with params in controllers. It is a bad bractice to put it in models. Basically anything that is considered view logic should be handled separately.
   def posting_image_params(html)
     tag_parse = -> (image, att) { image.match(/#{att}="(.+?)"/) }
     tag_attributes = {}
 
     %w[alt src data-image].each do |attribute|
       data = tag_parse.(html, attribute)
+      #What is data is nil here? This is not a good way to handle this. 
       unless data.nil?
         tag_attributes[attribute] = data[1] unless data.size < 2
       end
