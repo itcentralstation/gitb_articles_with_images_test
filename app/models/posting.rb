603bb30c9296fd48bacc840283cf8ffb212d9709
regsii <regsii@users.noreply.github.com>
2025-09-12 15:08:25 +0300

    refactor posting.rb

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -2,16 +2,19 @@ class Posting < ApplicationRecord
 
   belongs_to :author,    class_name: 'User', foreign_key: 'user_id'
   belongs_to :editor,    class_name: 'User', foreign_key: 'editor_id'
-  
-  def article_with_image
-    return type if type != 'Article'
 
-    figure_start = body.index('<figure')
-    figure_end = body.index('</figure>')
-    return "#{figure_start}_#{figure_end}" if figure_start.nil? || figure_end.nil?
+  def article_with_image(type, body)
+    return { error: "type (#{type}) is not Article" } if type != 'Article'
 
-    image_tags = body[figure_start...figure_end + 9]
-    return 'not include <img' unless image_tags.include?('<img')
+    figure_start_tag = '<figure'
+    figure_end_tag = '</figure>'
+    figure_start = body.index(figure_start_tag)
+    figure_end = body.index(figure_end_tag)
+
+    return { error: "<figure> ... </figure> not found" } if figure_start.nil? || figure_end.nil?
+
+    image_tags = body[figure_start...figure_end + figure_end_tag.length]
+    return { error: "`<img` not found" } unless image_tags.include?('<img')
 
     posting_image_params(image_tags)
   end
@@ -24,11 +27,11 @@ class Posting < ApplicationRecord
 
     %w[alt src data-image].each do |attribute|
       data = tag_parse.(html, attribute)
-      unless data.nil?
-        tag_attributes[attribute] = data[1] unless data.size < 2
-      end
+      tag_attributes[attribute] = data[1] unless (data.nil? || data[1].nil?)
     end
-    # tag_parse
+
+    return { error: 'no src for the image' } unless tag_attributes["src"]
+
     tag_attributes
   end
 end
